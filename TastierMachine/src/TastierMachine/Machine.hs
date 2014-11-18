cec3fbf6521fc9d68433e8240b3b701b6a837e02
caseyr4 <caseyr4@tcd.ie>
2014-11-18 16:52:43 +0000

    still fiddling with tell

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -213,7 +213,8 @@ run = do
           let charList = elems charArray
           map (tell $ show) charList
           --}
-          tell $ [show $ smem ! (rtp-1),show $ smem ! (rtp-1)]
+          let test = [1,2,3,4]
+          tell $ map (show) test
           put $ machine { rpc = rpc + 1, rtp = rtp - 1 }
           run
 
