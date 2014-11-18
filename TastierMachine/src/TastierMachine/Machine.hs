f2177b271e2c57f80791e081a86e2e2b1a713c9a
caseyr4 <caseyr4@tcd.ie>
2014-11-18 18:26:55 +0000

    debug

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -225,7 +225,7 @@ run = do
           let endAddr = stAddr + (length - 1)
           let valueList = map (dmem!) [stAddr..endAddr]
           let charList = map (chr . fromIntegral) valueList
-          tell $ map (show) charList
+          tell $ map (show) valueList
           put $ machine { rpc = rpc + 1, rtp = rtp - 1 }
           run
 
