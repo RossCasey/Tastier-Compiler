cffdb8b870d726683f76ec3d55c0119533232761
caseyr4 <caseyr4@tcd.ie>
2014-11-18 16:49:00 +0000

    still fiddling with tell

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -206,12 +206,14 @@ run = do
           run
 
         Instructions.WriteStr  -> do
-          let ptAddr = smem ! (rtp-1)
+          {--let ptAddr = smem ! (rtp-1)
           let length = dmem ! ptAddr
           let addr = ptAddr + 1
           let charArray = ixmap (addr,addr+(length-1)) (\i->i) dmem
           let charList = elems charArray
           map (tell $ show) charList
+          --}
+          tell $ [show $ smem ! (rtp-1)]
           put $ machine { rpc = rpc + 1, rtp = rtp - 1 }
           run
 
