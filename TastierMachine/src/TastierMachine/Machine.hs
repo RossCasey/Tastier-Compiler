28951b1acc70a0c724f6ca16f8296375cd3df95c
caseyr4 <caseyr4@tcd.ie>
2014-11-19 12:30:58 +0000

    debugging WriteStr

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -275,7 +275,7 @@ run = do
 
 
           let strNoQuotes = filter (/='\"') combStr --}
-          tell $ betterString
+          tell $ [betterString]
           put $ machine { rpc = rpc + 1, rtp = rtp - (1 + numArgs) }
           run
 
