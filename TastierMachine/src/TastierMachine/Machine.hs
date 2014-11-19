9f8b19d118b68c7ea2c4230958655b35c32a2260
caseyr4 <caseyr4@tcd.ie>
2014-11-19 11:19:10 +0000

    debugging WriteStr

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -251,35 +251,10 @@ run = do
           put $ machine { rpc = rpc + 1, rtp = rtp - 1 }
           run
 
-        {--
-        Instructions.WriteMul  -> do
-          let numArgs = (fromIntegral(smem ! (rtp-1))) * 2
-          let startArg = (fromIntegral(rtp - 2))
-          let finishArg = (fromIntegral(rtp - (numArgs + 1)))
-          let args = map (dmem!) [startArg..finishArg]
-          let args1 = [1,2,3]
-          let args2 = [(smem ! 8), (smem ! 7), (smem ! 6), (smem ! 5)]
-
-
-          let argsInt = map (fromIntegral) args
-          let typeList = everyEven argsInt
-          let valueList = everyOdd argsInt
-          let tupleList = zip typeList valueList
-          let memList = elems dmem
-          let strings = map (\x -> (valueOrPointer x memList)) tupleList
-          let combStr = show strings
-          let strNoQuotes = filter (/='\"') combStr --}
-
-
-          tell $ [show args]
-          put $ machine { rpc = rpc + 1, rtp = rtp - (1 + numArgs) }
-          run
-        --}
-
         Instructions.WriteMul  -> do
           let args = map (smem!) [8..5]
           tell $ [show args]
-          put $ machine { rpc = rpc + 1, rtp = rtp - (1 + numArgs) }
+          put $ machine { rpc = rpc + 1, rtp = rtp - (1 + 2) }
 
         Instructions.Leave  -> do
           {-
