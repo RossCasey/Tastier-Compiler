c687c08e905be1bc4dd4a2f43ff88e7debcccafd
caseyr4 <caseyr4@tcd.ie>
2014-12-23 22:13:09 +0000

    Changed implementation of records

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -300,7 +300,7 @@ run = do
           let address = (smem ! (rtp-1))
           let adjustedMemory = address - 3
           let result = dmem ! adjustedMemory
-          tell $ show result
+          tell $ [show result]
           put $ machine { rpc = rpc + 1, rtp = rtp - 1, smem = (smem // [(rtp-1, result)]) }
           run
 
