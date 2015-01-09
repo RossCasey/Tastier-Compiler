b7c672ecffdfd7334b5d886b0bd991e8066d2294
caseyr4 <caseyr4@tcd.ie>
2015-01-09 18:23:44 +0000

    Chasing memload bug

--- TastierMachine/src/TastierMachine/Machine.hs
+++ TastierMachine/src/TastierMachine/Machine.hs
@@ -240,7 +240,7 @@ run = do
 
         Instructions.WriteStr  -> do
           {-- Get pointer from stack --}
-          error $ show rtp
+
           let ptAddr = fromIntegral $ smem ! (rtp-1)
           error $ show rtp
           {-- Convert data memory to list --}
